8f020a1f6f07a8084d37f193ea8dd43f289da6ba
mayanddangi <67086304+mayanddangi@users.noreply.github.com>
2021-01-13 09:29:22 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,37 +1,13 @@
-'''
 from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get('https://web.whatsapp.com/')
-
-name = input('Enter the name of user or group : ')
-msg = input('Enter your message : ')
-count = int(input('Enter the count : '))
-
-input('Enter anything after scanning QR code')
-
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
-
-msg_box = driver.find_element_by_class_name('DuUXI')
-
-for i in range(count):
-    msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name('_3qpzV')
-    button.click()
-'''
-
-from selenium import webdriver
-
 driver = webdriver.Chrome()
 
 driver.get('https://web.whatsapp.com')
 
 driver.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
-inputString = input("Enter message to send: ")
+message = input("Enter message to send: ")
 
 for i in range(0,1000):
-    driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(inputString)
+    driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(message)
     driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
 
     
